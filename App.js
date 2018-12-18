6e507e6ffb03fad99a0294ebd14bb319230bf48b
olusola <olusola2509@gmail.com>
2018-12-18 14:05:08 +0000

    get current location lat and long

--- App.js
+++ App.js
@@ -1,11 +1,42 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-import MapView from 'react-native-maps';
+import {StyleSheet, Text, View} from 'react-native';
 export default class App extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      latitude: null,
+      longitude: null,
+      error: null
+    }
+  }
+
+  componentDidMount() {
+    navigator.geolocation.watchPosition((position) => {
+      this.setState({
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+        error: null
+      })
+    },
+    (error) => this.setState({ error: error.message }),
+    { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+    )
+  }
+
+  componentWillUnmount() {
+    navigator.geolocation.clearWatch(this.watchId);
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Where am i?</Text>
+        <View style={{ flexGrow: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <Text style={styles.welcome}>You are here </Text>
+          <Text>Latitude: {this.state.latitude}</Text>
+          <Text>Longitude: {this.state.longitude}</Text>
+          {this.state.error ? <Text>Error: {this.state.error}</Text> : null}
+        </View>
       </View>
     );
   }
